8b4d873d4e42ad21da789fc21086ec0e9ab24c8b
samidarko <samidarko@gmail.com>
2020-10-18 19:38:50 +0200

    Go: RNA transcription refactoring

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,19 +1,18 @@
 package strand
 
-// "ACGTU" => [65 67 71 84 85]
-var dnaRnaMapping = map[uint8]uint8{
-	65: 85, // A => U
-	67: 71, // C => G
-	71: 67, // G => C
-	84: 65, // T => A
+var dnaRnaMapping = map[rune]rune{
+	'A': 'U',
+	'C': 'G',
+	'G': 'C',
+	'T': 'A',
 }
 
 // ToRNA Given a DNA strand, return its RNA complement (per RNA transcription)
 func ToRNA(dna string) string {
 
-	dnaBytes := []byte(dna)
-	for i, _ := range dnaBytes {
-		dnaBytes[i] = dnaRnaMapping[dnaBytes[i]]
+	dnaBytes := make([]rune, len(dna))
+	for i, r := range dna {
+		dnaBytes[i] = dnaRnaMapping[r]
 	}
 	return string(dnaBytes)
 }
