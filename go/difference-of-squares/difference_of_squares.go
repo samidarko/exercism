fd1eff553545067a928a639d39a5e6003710dde5
samidarko <samidarko@gmail.com>
2021-01-12 19:28:47 +0100

    Go: Difference Of Squares: maths tricks

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -3,25 +3,14 @@ package diffsquares
 // SquareOfSum square the sum of the first n natural numbers
 func SquareOfSum(n int) int {
 
-	squareOfSum, naturalNumber := 0, 1
-	for i := 0; i < n; i++ {
-		squareOfSum += naturalNumber
-		naturalNumber += 1
-	}
+	squareOfSum := (n * (n + 1)) / 2
 
 	return squareOfSum * squareOfSum
 }
 
 // SumOfSquares sum the squares of the first n natural numbers
 func SumOfSquares(n int) int {
-	sumOfSquares, naturalNumber := 0, 1
-
-	for i := 0; i < n; i++ {
-		sumOfSquares += naturalNumber * naturalNumber
-		naturalNumber += 1
-	}
-
-	return sumOfSquares
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
