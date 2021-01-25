d047c76963eacceaadda56f887d1733b7d4c75d5
samidarko <samidarko@gmail.com>
2021-01-25 19:32:03 +0100

    Go: Luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,43 @@
 package luhn
 
+import "strings"
+
 func Valid(s string) bool {
-	return true
+	runes := []rune(strings.TrimSpace(s))
+	length := len(runes)
+
+	if length < 2 {
+		return false
+	}
+	isSecondDigit := false
+	digitsSum := 0
+
+	var r rune
+
+	for i := length - 1; i >= 0; i-- {
+		r = runes[i]
+
+		switch r {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			digit := int(r - '0')
+			if isSecondDigit {
+				doubledDigit := digit * 2
+				if doubledDigit > 9 {
+					digit = doubledDigit - 9
+				} else {
+					digit = doubledDigit
+				}
+				isSecondDigit = false
+			} else {
+				isSecondDigit = true
+			}
+			digitsSum += digit
+		case ' ':
+		default:
+			return false
+
+		}
+
+	}
+	return digitsSum%10 == 0
 }
