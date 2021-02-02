f39de0244b76286d3e02b1a8671b14382a04301e
samidarko <samidarko@gmail.com>
2021-02-02 10:22:29 +0100

    Go: Luhn: removed switch

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,10 +1,13 @@
 package luhn
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // Valid given a number determine whether or not it is valid per the Luhn formula
 func Valid(s string) bool {
-	runes := []rune(strings.TrimSpace(s))
+	runes := []rune(strings.Replace(s, " ", "", -1))
 	length := len(runes)
 
 	if length < 2 {
@@ -18,26 +21,23 @@ func Valid(s string) bool {
 	for i := length - 1; i >= 0; i-- {
 		r = runes[i]
 
-		switch r {
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			digit := int(r - '0')
-			if isSecondDigit {
-				doubledDigit := digit * 2
-				if doubledDigit > 9 {
-					digit = doubledDigit - 9
-				} else {
-					digit = doubledDigit
-				}
-				isSecondDigit = false
-			} else {
-				isSecondDigit = true
-			}
-			digitsSum += digit
-		case ' ':
-		default:
+		if !unicode.IsDigit(r) {
 			return false
-
 		}
+
+		digit := int(r - '0')
+		if isSecondDigit {
+			doubledDigit := digit * 2
+			if doubledDigit > 9 {
+				digit = doubledDigit - 9
+			} else {
+				digit = doubledDigit
+			}
+			isSecondDigit = false
+		} else {
+			isSecondDigit = true
+		}
+		digitsSum += digit
 
 	}
 	return digitsSum%10 == 0
