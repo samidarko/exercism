492324987f67916f553308143de613bc1352dd01
samidarko <samidarko@gmail.com>
2021-02-21 20:32:58 +0100

    Go: Pythagorean Triplet

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -3,17 +3,14 @@ package pythagorean
 type Triplet [3]int
 
 func Range(min, max int) []Triplet {
-
-	var c int
 	var triplets []Triplet
 
 	for a := min; a <= max; a++ {
-
 		for b := a + 1; b <= max; b++ {
-			c = max - a - b
-
-			if a < b && b < c {
-				triplets = append(triplets, Triplet{a, b, c})
+			for c := b + 1; c <= max; c++ {
+				if (a*a + b*b) == c*c {
+					triplets = append(triplets, Triplet{a, b, c})
+				}
 			}
 		}
 	}
@@ -22,21 +19,16 @@ func Range(min, max int) []Triplet {
 }
 
 func Sum(p int) []Triplet {
-
 	var c int
 	var triplets []Triplet
 
 	for a := 1; a <= p; a++ {
-
 		for b := a + 1; b <= p; b++ {
 			c = p - a - b
-
-			if (a*a + b*b) == c*c {
+			if (a*a+b*b) == c*c && (a+b+c) == p {
 				triplets = append(triplets, Triplet{a, b, c})
 			}
-
 		}
-
 	}
 
 	return triplets
