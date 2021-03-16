0a6f60509a295cd132553e41def8f855291ab7b0
samidarko <samidarko@gmail.com>
2021-03-16 19:41:55 +0100

    Go: Grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,17 +2,12 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Total should return the total number of grains for chessboard (64 squares)
 func Total() uint64 {
-	var totalGrains uint64
-	for position := 1; position < 65; position++ {
-		grains, _ := Square(position)
-		totalGrains += grains
-	}
-	return totalGrains
+	grains, _ := Square(64)
+	return grains*2 - 1
 }
 
 // Square should return the number of grains for a square position or an error
@@ -21,5 +16,5 @@ func Square(position int) (uint64, error) {
 		return 0, errors.New("square position should be between 1 and 64")
 	}
 	// Terms  in  the  Doubling  sequence  can  be  computed  by  the  function: d(n) = 2^n (for all natural numbers n)
-	return uint64(math.Pow(2.0, float64(position-1))), nil
+	return 1 << (position - 1), nil
 }
