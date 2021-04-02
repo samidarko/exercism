c9145a48ffab4cb46b2dd86d1a196d494dbb6fea
samidarko <samidarko@gmail.com>
2021-04-02 11:30:06 +0200

    Go: Phone Number

---
+++ go/phone-number/phone_number.go
@@ -0,0 +1,63 @@
+package phonenumber
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
+// Number return phone number
+func Number(input string) (string, error) {
+	input, err := sanitizeInput(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	return input, nil
+}
+
+// AreaCode return area code
+func AreaCode(input string) (string, error) {
+	input, err := sanitizeInput(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	return input[:3], nil
+}
+
+// Format return a formatted phone number with its area code
+func Format(input string) (string, error) {
+	input, err := sanitizeInput(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("(%s) %s-%s", input[:3], input[3:6], input[6:]), nil
+}
+
+func sanitizeInput(input string) (string, error) {
+	reg, _ := regexp.Compile("[^0-9]")
+	input = reg.ReplaceAllString(input, "")
+
+	if len(input) > 0 && input[0] == '1' {
+		input = input[1:] // removes '1'
+	}
+
+	if len(input) != 10 {
+		return "", errors.New("invalid length")
+	}
+
+	if int(input[0]-'0') < 2 {
+		return "", errors.New("invalid area code")
+	}
+
+	if int(input[3]-'0') < 2 {
+		return "", errors.New("invalid number")
+	}
+
+	return input, nil
+}
