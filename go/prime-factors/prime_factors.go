aad502e59059a0b47b6a17f7287f54e7bc296d06
samidarko <samidarko@gmail.com>
2021-06-29 15:52:40 +0200

    Go: Prime Factors (simplification)

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,21 +1,17 @@
 package prime
 
-import (
-	"math/big"
-)
-
 // Factors compute the prime factors of a given natural number
 func Factors(input int64) []int64 {
 
 	factors := make([]int64, 0)
 
-	for i := int64(2); input > 1; {
-		if big.NewInt(i).ProbablyPrime(0) && input%i == 0 {
-			factors = append(factors, i)
-			input /= i
+	for factor := int64(2); input > 1; {
+		if input%factor == 0 {
+			factors = append(factors, factor)
+			input /= factor
 			continue
 		}
-		i++
+		factor++
 	}
 	return factors
 }
