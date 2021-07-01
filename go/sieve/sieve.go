e81c57b2ff8085e57dd1853741acd53b547d9325
samidarko <samidarko@gmail.com>
2021-07-01 17:03:30 +0200

    Go: Sieve (improvements)

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -9,28 +9,22 @@ type Number struct {
 // Sieve find all the primes from 2 up to a given number
 func Sieve(limit int) []int {
 
-	// create `numbers` an int series from 2 to limit (hence -1)
-	numbers := make([]Number, limit-1, limit-1)
-	for i := range numbers {
-		numbers[i] = Number{isPrime: true, value: i + 2}
+	// create a sequence of numbers from 2 to limit (hence -1)
+	sequence := make([]Number, limit-1, limit-1)
+	for i := range sequence {
+		sequence[i] = Number{isPrime: true, value: i + 2}
 	}
 
 	// apply algorithm
-	for _, number := range numbers {
+	primes := make([]int, 0)
+	for _, number := range sequence {
 		if number.isPrime {
-			for n := number.value + number.value; n <= len(numbers)+1; n = n + number.value {
-				numbers[n-2].isPrime = false
+			primes = append(primes, number.value)
+			for n := number.value + number.value; n <= len(sequence)+1; n = n + number.value {
+				sequence[n-2].isPrime = false
 			}
 		}
 	}
 
-	// compute result, a list of primes if found
-	result := make([]int, 0)
-	for _, number := range numbers {
-		if number.isPrime {
-			result = append(result, number.value)
-		}
-	}
-
-	return result
+	return primes
 }
