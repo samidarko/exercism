2a22280e7405cf7e220777e803fd86143b948348
samidarko <samidarko@gmail.com>
2022-01-22 11:00:59 +0100

    Go: Pascals Triangle (simplified)

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,20 +1,14 @@
 package pascal
 
 // Triangle compute Pascal's triangle up to a given number of rows
-func Triangle(n int) (triangle [][]int) {
-	triangle = make([][]int, n)
-	triangle[0] = []int{1}
-	if n == 1 {
-		return
-	}
-	for i := 1; i < n; i++ {
-		previous := triangle[i-1]
-		current := make([]int, i+1)
-		current[0], current[i] = 1, 1
+func Triangle(n int) [][]int {
+	triangle := make([][]int, n)
+	for i := 0; i < n; i++ {
+		triangle[i] = make([]int, i+1)
+		triangle[i][0], triangle[i][i] = 1, 1
 		for j := 1; j < i; j++ {
-			current[j] = previous[j-1] + previous[j]
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
 		}
-		triangle[i] = current
 	}
-	return
+	return triangle
 }
