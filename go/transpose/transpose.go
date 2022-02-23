82a72158167254da70833a2b442a6299439f47bc
samidarko <samidarko@gmail.com>
2022-02-23 22:12:18 +0100

    Go: Transpose (simplified)

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -1,30 +1,22 @@
 package transpose
 
+import (
+	"fmt"
+	"strings"
+)
+
+// Transpose an input text to an output text
 func Transpose(input []string) []string {
-	temp := make([][]rune, 0)
+	result := make([]string, 0)
 
 	for i, s := range input {
 		for j, r := range s {
-			if j >= len(temp) {
-				arr := make([]rune, 0)
-				for k := 0; k < i; k++ {
-					arr = append(arr, ' ')
-				}
-				arr = append(arr, r)
-				temp = append(temp, arr)
+			if j < len(result) {
+				result[j] = fmt.Sprint(result[j], strings.Repeat(" ", i-len(result[j])), string(r))
 			} else {
-				for k := len(temp[j]); k < i; k++ {
-					temp[j] = append(temp[j], ' ')
-				}
-				temp[j] = append(temp[j], r)
+				result = append(result, fmt.Sprint(strings.Repeat(" ", i), string(r)))
 			}
 		}
-	}
-
-	result := make([]string, 0)
-
-	for _, r := range temp {
-		result = append(result, string(r))
 	}
 
 	return result
