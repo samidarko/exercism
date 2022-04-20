5a3a4d0115912de7bac7e51492b5f0a5e9ae2f8f
samidarko <samidarko@gmail.com>
2022-04-20 23:45:22 +0200

    Go: Food Chain

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -5,64 +5,64 @@ import (
 	"strings"
 )
 
-var verses = []string{``,
-
-	`I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a horse.
-She's dead, of course!`,
+var animals = []string{
+	"fly",
+	"spider",
+	"bird",
+	"cat",
+	"dog",
+	"goat",
+	"cow",
+	"horse",
 }
 
+func getComment(animal string) string {
+	switch animal {
+	case "bird":
+		return "How absurd to swallow a bird!"
+	case "cat":
+		return "Imagine that, to swallow a cat!"
+	case "dog":
+		return "What a hog, to swallow a dog!"
+	case "goat":
+		return "Just opened her throat and swallowed a goat!"
+	case "cow":
+		return "I don't know how she swallowed a cow!"
+	default:
+		return ""
+	}
+}
+
+// Verse returns a verse
 func Verse(v int) string {
-	return verses[v]
+	var output strings.Builder
+
+	currentAnimal := animals[v-1]
+	output.WriteString(fmt.Sprintf("I know an old lady who swallowed a %s.\n", currentAnimal))
+	if currentAnimal == "horse" {
+		output.WriteString(fmt.Sprint("She's dead, of course!"))
+		return output.String()
+	}
+	if currentAnimal != "fly" {
+		if comment := getComment(currentAnimal); comment != "" {
+			output.WriteString(fmt.Sprintln(getComment(currentAnimal)))
+		}
+		if currentAnimal == "spider" {
+			output.WriteString(fmt.Sprintf("It wriggled and jiggled and tickled inside her.\n"))
+		}
+		for i := v - 1; i >= 1; i-- {
+			output.WriteString(fmt.Sprintf("She swallowed the %s to catch the %s", animals[i], animals[i-1]))
+			if animals[i-1] == "spider" {
+				output.WriteString(fmt.Sprint(" that wriggled and jiggled and tickled inside her"))
+			}
+			output.WriteString(fmt.Sprintln("."))
+		}
+	}
+	output.WriteString("I don't know why she swallowed the fly. Perhaps she'll die.")
+	return output.String()
 }
 
+// Verses returns verses within start and end inclusive
 func Verses(start, end int) string {
 	var output strings.Builder
 
@@ -74,6 +74,7 @@ func Verses(start, end int) string {
 	return output.String()
 }
 
+// Song returns the song
 func Song() string {
 	return Verses(1, 8)
 }
