58aa299020910e67bab2d93604b753d5908c03f4
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-19 15:04:58 +0200

    Rust: RPN Calculator

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -9,46 +9,31 @@ pub enum CalculatorInput {
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let stack: &mut Vec<i32> = &mut Vec::new();
-    fn get_values(s: &mut Vec<i32>) -> Option<(i32, i32)> {
-        if s.len() > 1 {
-            let b = s.pop().unwrap();
-            let a = s.pop().unwrap();
-            Some((a, b))
-        } else {
-            None
-        }
-    }
+
     for input in inputs {
         match input {
             CalculatorInput::Value(value) => stack.push(*value),
-            CalculatorInput::Add => {
-                if let Some((a, b)) = get_values(stack) {
-                    stack.push(a + b)
-                } else {
-                    return None
-                }
+            CalculatorInput::Add if stack.len() > 1 => {
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a + b)
             },
-            CalculatorInput::Subtract => {
-                if let Some((a, b)) = get_values(stack) {
-                    stack.push(a - b)
-                } else {
-                    return None
-                }
+            CalculatorInput::Subtract if stack.len() > 1 => {
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a - b)
             },
-            CalculatorInput::Multiply => {
-                if let Some((a, b)) = get_values(stack) {
-                    stack.push(a * b)
-                } else {
-                    return None
-                }
+            CalculatorInput::Multiply if stack.len() > 1 => {
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a * b)
             },
-            CalculatorInput::Divide => {
-                if let Some((a, b)) = get_values(stack) {
-                    stack.push(a / b)
-                } else {
-                    return None
-                }
+            CalculatorInput::Divide if stack.len() > 1 => {
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a / b)
             },
+            _ => return None
         }
     }
 
