2c64a0bc299dc9cd76cae1b14a952633d914dfb6
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-21 19:53:19 +0200

    Rust: Leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
     match year {
         _year if year % 4 == 0 && year % 100 == 0 && year % 400 != 0 => false,
-        _year if year % 4 == 0  => true,
-        _year  => false,
+        _year if year % 4 == 0 => true,
+        _year => false,
     }
 }
