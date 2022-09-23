5c33bb76b783c50bdbaff36abac6c1af8b040bb8
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-23 14:22:25 +0200

    Rust: Grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -6,6 +6,8 @@ pub fn square(position: u32) -> u64 {
 }
 
 pub fn total() -> u128 {
+    // https://exercism.org/tracks/rust/exercises/grains/solutions/philip98
+    // is returning `u64::max_value()` which is equal to `square(64)` and allow to keep u64 as return type
     let grains: u128 = square(64) as u128;
     grains * 2 - 1
 }
