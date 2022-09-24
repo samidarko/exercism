6839926557a560f0ec1cd5fdf16117ff90ef2531
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-24 10:46:21 +0200

    Rust: Nth Prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,7 +1,7 @@
 use std::ops::Not;
 
 pub fn nth(n: u32) -> u32 {
-    if let Some(last_prime) = (2..).filter(|x| is_prime(*x)).take(1 + n as usize).last() {
+    if let Some(last_prime) = (2..).filter(|x| is_prime(*x)).nth(n as usize) {
         last_prime
     } else {
         2
