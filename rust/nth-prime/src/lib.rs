6acbb39710fc210cd52054332f226b38c4acf89a
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-24 10:42:50 +0200

    Rust: Nth Prime

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,14 @@
+use std::ops::Not;
+
+pub fn nth(n: u32) -> u32 {
+    if let Some(last_prime) = (2..).filter(|x| is_prime(*x)).take(1 + n as usize).last() {
+        last_prime
+    } else {
+        2
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    let m = n / 2;
+    (2..=m).any(|i| n % i == 0).not()
+}
