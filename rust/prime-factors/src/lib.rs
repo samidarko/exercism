50094c807c9f8df87d3a8ad58058ae5cd2cf2813
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-25 10:14:43 +0200

    Rust: Prime Factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut input: u64 = n;
+    let mut n: u64 = n;
     let mut factor: u64 = 2;
     let mut factors: Vec<u64> = Vec::new();
 
-    while input > 1 {
-        if input % factor == 0 {
+    while n > 1 {
+        if n % factor == 0 {
             factors.push(factor);
-            input /= factor;
+            n /= factor;
             continue;
         }
         factor += 1;
