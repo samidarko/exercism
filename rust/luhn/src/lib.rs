9a23cb9489aa3141975d3db86e155dbb76aacca9
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-25 10:19:00 +0200

    Rust: Luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -13,11 +13,14 @@ pub fn is_valid(code: &str) -> bool {
         |(is_second_digit, digits_sum), c| {
             if let Some(digit) = c.to_digit(10) {
                 match digit * 2 {
-                    doubled_digit if is_second_digit && doubled_digit > 9 => ControlFlow::Continue((!is_second_digit, digits_sum + doubled_digit - 9)),
-                    doubled_digit if is_second_digit => ControlFlow::Continue((!is_second_digit, digits_sum + doubled_digit)),
+                    doubled_digit if is_second_digit && doubled_digit > 9 => {
+                        ControlFlow::Continue((!is_second_digit, digits_sum + doubled_digit - 9))
+                    }
+                    doubled_digit if is_second_digit => {
+                        ControlFlow::Continue((!is_second_digit, digits_sum + doubled_digit))
+                    }
                     _ => ControlFlow::Continue((!is_second_digit, digits_sum + digit)),
                 }
-
             } else {
                 // c is not a digit
                 ControlFlow::Break((is_second_digit, digits_sum))
