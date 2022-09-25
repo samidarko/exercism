2d86afaecef618b28952fa812e53c27ca6175909
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-25 20:12:33 +0200

    Rust: Beer Song

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -3,7 +3,7 @@ pub fn verse(n: u32) -> String {
         0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
-        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1)
+        _ => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n", n, n-1)
     }
 }
 
