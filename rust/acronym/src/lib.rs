93ad5cca788b17f23358816872f9e42e062e63c5
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-26 23:49:16 +0200

    Rust: Acronym

---
+++ rust/acronym/src/lib.rs
@@ -0,0 +1,28 @@
+use regex::{Captures, Regex};
+
+pub fn abbreviate(phrase: &str) -> String {
+    phrase
+        .replace("_", "")
+        .replace("-", " ")
+        .split_whitespace()
+        .flat_map(|word| un_camel_case(word))
+        .map(|word| word.chars().nth(0).unwrap().to_uppercase().to_string())
+        .collect::<String>()
+}
+
+fn un_camel_case(string: &str) -> Vec<String> {
+    let re = Regex::new(r"(?:[A-Z][a-z]+)").unwrap();
+    let caps: Vec<Captures> = re.captures_iter(string).collect();
+
+    if caps.len() > 1 {
+        return caps
+            .iter()
+            .map(|c| c[0].to_string())
+            .collect::<Vec<String>>();
+    }
+
+    string
+        .split_whitespace()
+        .map(&str::to_string)
+        .collect::<Vec<String>>()
+}
