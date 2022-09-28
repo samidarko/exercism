e12fc22932f5c402b79781ea39263b70ad403cf3
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-09-28 14:54:04 +0200

    Rust: Etl (simplification)

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -2,19 +2,6 @@ use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     h.iter()
-        .flat_map(|(x, v)| {
-            v.iter()
-                .map(|c| {
-                    (
-                        c.to_lowercase()
-                            .collect::<Vec<_>>()
-                            .first()
-                            .unwrap()
-                            .clone(),
-                        *x,
-                    )
-                })
-                .collect::<Vec<(char, i32)>>()
-        })
-        .collect::<BTreeMap<char, i32>>()
+        .flat_map(|(x, v)| v.iter().map(|c| (c.to_ascii_lowercase(), *x)))
+        .collect()
 }
