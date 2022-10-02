32f1b7bba7680deadb575ed4edcc89b33de11e96
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-02 20:45:09 +0200

    Rust: Sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -5,20 +5,19 @@ pub struct Number {
 }
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut numbers: Vec<Number> = vec![];
-    for i in 0..upper_bound - 1 {
-        numbers.push(Number {
-            value: i + 2,
+    let mut numbers: Vec<Number> = (0..upper_bound - 1)
+        .map(|value| Number {
+            value: value + 2,
             is_prime: true,
         })
-    }
+        .collect();
 
     let mut primes: Vec<u64> = vec![];
 
     for i in 0..numbers.len() {
         if numbers[i].is_prime {
             primes.push(numbers[i].value);
-            let mut n: usize = (numbers[i].value + numbers[i].value) as usize;
+            let mut n: usize = (numbers[i].value * 2) as usize;
             while n <= numbers.len() + 1 {
                 numbers[n - 2].is_prime = false;
                 n += numbers[i].value as usize;
