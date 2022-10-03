6c019be42a9a3bed42f4b9a32c9c6def59b683f5
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-03 23:20:49 +0200

    Rust: Nucleotide Count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,25 +1,15 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    match nucleotide_counts(dna) {
-        Ok(counts) => match counts.get(&nucleotide) {
-            Some(count) => Ok(*count),
-            None => Err(nucleotide),
-        },
-        Err(c) => Err(c),
-    }
+    let counts = nucleotide_counts(dna)?;
+    counts.get(&nucleotide).copied().ok_or(nucleotide)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
     let mut counts: HashMap<char, usize> = HashMap::from([('A', 0), ('C', 0), ('G', 0), ('T', 0)]);
 
     for c in dna.chars() {
-        match counts.get(&c) {
-            Some(count) => {
-                counts.insert(c, *count + 1);
-            }
-            None => return Err(c),
-        }
+        counts.get_mut(&c).map(|count| *count += 1).ok_or(c)?;
     }
 
     Ok(counts)
