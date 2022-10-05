b3fd797adec3ee1c3cc248ee3f646fb58fb39d45
samidarko <samidarko@gmail.com>
2022-10-05 10:52:44 +0200

    Rust: Allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,51 @@
+use enum_iterator::{all, Sequence};
+
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Sequence)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+fn get_allergen_code(allergen: &Allergen) -> u32 {
+    use Allergen::*;
+    match allergen {
+        Eggs => 1,
+        Peanuts => 2,
+        Shellfish => 4,
+        Strawberries => 8,
+        Tomatoes => 16,
+        Chocolate => 32,
+        Pollen => 64,
+        Cats => 128,
+    }
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self { score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        let code = get_allergen_code(allergen);
+        self.score & code == code
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        all::<Allergen>()
+            .collect::<Vec<_>>()
+            .iter()
+            .filter(|a| self.is_allergic_to(*a))
+            .copied()
+            .collect()
+    }
+}
