4bd2405da8c6e60c2115844a34f2a52ff17857e1
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-06 23:07:17 +0200

    Rust: Protein Translation

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -10,33 +10,16 @@ impl<'a> CodonsInfo<'a> {
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        if rna.len() < 9 {
-            return None;
-        }
-        let codons = rna
-            .chars()
-            .take(9)
+        rna.chars()
             .collect::<Vec<char>>()
             .chunks(3)
-            .map(String::from_iter)
-            .collect::<Vec<String>>();
-
-        let mut codon_names: Vec<&'a str> = vec![];
-
-        codons.iter().for_each(|codon| match self.name_for(&codon) {
-            Some(name) if name != "stop codon" => codon_names.push(name),
-            _ => (),
-        });
-
-        if codon_names.is_empty() {
-            None
-        } else {
-            Some(codon_names)
-        }
+            .map(|chars| self.name_for(&chars.iter().collect::<String>()))
+            .take_while(|result| result.is_none() || result.unwrap() != "stop codon")
+            .collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    let codon_name = pairs.iter().copied().collect::<HashMap<&'a str, &'a str>>();
+    let codon_name = pairs.iter().copied().collect();
     CodonsInfo { codon_name }
 }
