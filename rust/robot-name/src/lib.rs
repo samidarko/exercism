0b2bbbb3bd226084e175437a560b2f86de20e006
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-07 23:17:36 +0200

    Rust: Robot Name

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,18 +1,12 @@
-use rand::{Rng, SeedableRng};
-use std::time::SystemTime;
-// use rand_core;
+#[macro_use]
+extern crate lazy_static;
+use rand::Rng;
+use std::collections::HashSet;
+use std::sync::Mutex;
 
-// fn all_names<'a>() -> Vec<String> {
-//     let mut names: Vec<String> = vec![];
-//     for c in 'A'..='Z' {
-//         for d in 'A'..='Z' {
-//             for n in 0..1_000 {
-//                 names.push(format!("{}{}{:03}", c, d, n));
-//             }
-//         }
-//     }
-//     names
-// }
+lazy_static! {
+    static ref USED_NAMES: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
+}
 
 const ALPHABET: [char; 26] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -30,18 +24,6 @@ impl Robot {
         }
     }
 
-    // pub fn all_names(self) -> Vec<String> {
-    //     let mut names: Vec<String> = vec![];
-    //     for c in 'A'..='Z' {
-    //         for d in 'A'..='Z' {
-    //             for n in 0..1_000 {
-    //                 names.push(format!("{}{}{:03}", c, d, n));
-    //             }
-    //         }
-    //     }
-    //     names
-    // }
-
     pub fn name(&self) -> &str {
         return self.name.as_str();
     }
@@ -52,31 +34,18 @@ impl Robot {
 }
 
 fn get_random_name() -> String {
-    let unix_ts: u64 = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_nanos() as u64;
-    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(unix_ts);
-    let first_char = rng.gen_range(0..26);
-    let second_char = rng.gen_range(0..26);
-    let number = rng.gen_range(0..1_000);
-    format!(
-        "{}{}{:03}",
-        ALPHABET[first_char], ALPHABET[second_char], number
-    )
+    let mut rng = rand::thread_rng();
+    let mut used_names = USED_NAMES.lock().unwrap();
+    loop {
+        let first_char = rng.gen_range(0..26);
+        let second_char = rng.gen_range(0..26);
+        let number = rng.gen_range(0..1_000);
+        let name = format!(
+            "{}{}{:03}",
+            ALPHABET[first_char], ALPHABET[second_char], number
+        );
+        if used_names.insert(name.clone()) {
+            return name;
+        }
+    }
 }
-
-// fn name_generator() -> impl Fn() -> String {
-//
-//     let mut names: Vec<String> = vec![];
-//     let mut index: usize = 0;
-//
-//     let function: fn() -> String = || {
-//
-//         match names.get(index) {
-//             Some(name) => name.clone(),
-//             None => panic!("no more names"),
-//         }
-//     };
-//     function
-// }
