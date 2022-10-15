6ba59fc73ec0dcb8c5ddad52b6f60ab94610a3e8
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-15 19:54:56 +0200

    Go: OCR Numbers

--- go/ocr-numbers/ocr_numbers.go
+++ go/ocr-numbers/ocr_numbers.go
@@ -5,9 +5,6 @@ import (
 	"strings"
 )
 
-// Digit represents 3 x 3 'ascii' number
-type Digit [3][3]rune
-
 // Recognize numbers on multiple lines
 func Recognize(input string) (output []string) {
 	lines := strings.Split(input, "\n")
@@ -16,11 +13,7 @@ func Recognize(input string) (output []string) {
 		line := lines[i+1 : i+4]
 		digits := ""
 		for j := 0; j+3 <= len(line[0]); j += 3 {
-			digit := Digit{
-				sliceToArray(line[0][j : j+3]),
-				sliceToArray(line[1][j : j+3]),
-				sliceToArray(line[2][j : j+3]),
-			}
+			digit := fmt.Sprint(line[0][j:j+3], line[1][j:j+3], line[2][j:j+3])
 			digits = fmt.Sprint(digits, recognizeDigit(digit))
 		}
 		output = append(output, digits)
@@ -29,41 +22,29 @@ func Recognize(input string) (output []string) {
 	return
 }
 
-func sliceToArray(slice string) [3]rune {
-	runes := []rune(slice)
-	return [3]rune{runes[0], runes[1], runes[2]}
-}
+func recognizeDigit(digit string) string {
 
-func recognizeDigit(digit Digit) string {
-
-	switch {
-	case digit[0][1] == ' ' && digit[2][0] == ' ' && digit[1][2] == '|':
-		if digit[1][1] == '_' {
-			return "4"
-		}
-		return "1"
-	case digit[2][1] == ' ' && digit[2][0] == ' ' && digit[1][2] == '|':
-		return "7"
-	case digit[1][1] == ' ' && digit[0][1] == '_' && digit[2][0] == '|':
+	switch digit {
+	case " _ | ||_|":
 		return "0"
-	case digit[1][0] == ' ' && digit[1][2] == '|':
-		if digit[2][0] == '|' {
-			return "2"
-		}
+	case "     |  |":
+		return "1"
+	case " _  _||_ ":
+		return "2"
+	case " _  _| _|":
 		return "3"
-
-	case digit[1][2] == ' ' && digit[1][0] == '|':
-		if digit[2][0] == '|' {
-			return "6"
-		}
+	case "   |_|  |":
+		return "4"
+	case " _ |_  _|":
 		return "5"
-
-	case digit[0][1] == '_' && digit[1][1] == '_':
-		if digit[2][0] == '|' {
-			return "8"
-		}
+	case " _ |_ |_|":
+		return "6"
+	case " _   |  |":
+		return "7"
+	case " _ |_||_|":
+		return "8"
+	case " _ |_| _|":
 		return "9"
-
 	default:
 		return "?"
 	}
