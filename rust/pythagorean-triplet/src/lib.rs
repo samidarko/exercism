9cde8aef19f25161ea526971f2d35b1a2f92cf76
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-17 22:48:03 +0200

    Rust: Pythagorean Triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -3,6 +3,6 @@ use std::collections::HashSet;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     (1..=sum)
         .flat_map(|a| (a + 1..=sum - a).map(move |b| [a, b, sum - (a + b)]))
-        .filter(|[a, b, c]| (a * a + b * b) == c * c && (a + b + c) == sum)
+        .filter(|[a, b, c]| (a.pow(2) + b.pow(2)) == c.pow(2) && (a + b + c) == sum)
         .collect()
 }
