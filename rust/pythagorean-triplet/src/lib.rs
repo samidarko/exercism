2a400aab9a3b22da3355f06d8779d3d2e1031484
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-17 22:33:50 +0200

    Rust: Pythagorean Triplet

---
+++ rust/pythagorean-triplet/src/lib.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+
+pub fn find(sum: u32) -> HashSet<[u32; 3]> {
+    let mut triplets = HashSet::new();
+    for a in 1..=sum {
+        for b in a + 1..=sum - a {
+            let c = sum - (a + b);
+            if (a * a + b * b) == c * c && (a + b + c) == sum {
+                triplets.insert([a, b, c]);
+            }
+        }
+    }
+    triplets
+}
