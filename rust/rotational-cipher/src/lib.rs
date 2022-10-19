8e256168950810fc7f487ca7994e7cecba6b712e
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-19 20:20:09 +0200

    Rust: Rational Cipher

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,5 +1,3 @@
-// use std::collections::HashMap;
-
 const ALPHABET: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -15,15 +13,9 @@ pub fn rotate(input: &str, key: i8) -> String {
                 .map(|i| {
                     let position = i as i8 + key;
                     let rotated = match key {
-                        k if k >= 0 && position >= 26 => {
-                            ALPHABET[(position - 26) as usize]
-                        }
-                        _ if position < 0 => {
-                            ALPHABET[(26 + position) as usize]
-                        }
-                        _ => {
-                            ALPHABET[position as usize]
-                        }
+                        k if k >= 0 && position >= 26 => ALPHABET[(position - 26) as usize],
+                        _ if position < 0 => ALPHABET[(26 + position) as usize],
+                        _ => ALPHABET[position as usize],
                     };
                     if c.is_uppercase() {
                         rotated.to_ascii_uppercase()
