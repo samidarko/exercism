058b0a2df27bc35db5705d996dc4e43bcd454b1b
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-22 12:33:59 +0200

    Rust: Binary Search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,25 +1,27 @@
 use std::cmp::Ordering;
 
-pub fn find<T>(array: &[T], key: T) -> Option<usize>
+pub fn find<T, U>(array: U, key: T) -> Option<usize>
 where
     T: Ord,
+    U: AsRef<[T]>,
 {
-    if array.is_empty() {
+    let array = array.as_ref();
+    if array.len() == 0 {
         return None;
     }
     let (mut left, mut right) = (0usize, array.len() - 1);
     while left <= right {
         let index = (left + right) / 2;
-        match array[index].cmp(&key) {
-            Ordering::Less => left = index + 1,
-            Ordering::Greater => {
+        match key.cmp(&array[index]) {
+            Ordering::Equal => return Some(index),
+            Ordering::Greater => left = index + 1,
+            Ordering::Less => {
                 let (value, overflowing) = index.overflowing_sub(1);
                 if overflowing {
                     return None;
                 }
                 right = value;
             }
-            Ordering::Equal => return Some(index),
         }
     }
     None
