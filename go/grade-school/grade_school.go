9b0db56a3c2a23ec909b54e713d01fa39ab98634
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-24 19:19:34 +0200

    Go: Grade School

--- go/grade-school/grade_school.go
+++ go/grade-school/grade_school.go
@@ -1,7 +1,9 @@
 package school
 
+import "sort"
+
 // School is school type
-type School map[string]int
+type School []Grade
 
 // Grade is grade type
 type Grade struct {
@@ -13,20 +15,30 @@ func New() *School {
 	return new(School)
 }
 
-func (s *School) Add(student string, grade int) {
-	(*s)[student] = grade
-}
-
-func (s *School) Grade(level int) (students []string) {
-	for student, l := range *s {
-		if level == l {
-			students = append(students, student)
+func (s *School) Add(student string, level int) {
+	for i, grade := range *s {
+		if grade.level == level {
+			// todo search if already exists
+			(*s)[i].students = append((*s)[i].students, student)
+			return
 		}
 	}
-	return
+	*s = append(*s, Grade{level: level, students: []string{student}})
+}
+
+func (s *School) Grade(level int) []string {
+	for _, grade := range *s {
+		if grade.level == level {
+			return grade.students
+		}
+	}
+	return []string{}
 }
 
 func (s *School) Enrollment() (grades []Grade) {
-
-	panic("Please implement the Enrollment function")
+	for i := range *s {
+		sort.Slice((*s)[i].students, func(a, b int) bool { return (*s)[i].students[a] < (*s)[i].students[b] })
+	}
+	sort.Slice(*s, func(a, b int) bool { return (*s)[a].level < (*s)[b].level })
+	return *s
 }
