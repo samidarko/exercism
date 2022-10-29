5ef8d11c16d514f3906e707572a2b5a459b6e28a
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-10-29 09:20:21 +0200

    Rust: Simple Cipher

--- rust/simple-cipher/src/lib.rs
+++ rust/simple-cipher/src/lib.rs
@@ -4,24 +4,31 @@ const A: u8 = 'a' as u8;
 const Z: u8 = 'z' as u8;
 
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    if key.is_empty() || key.chars().any(|c|c.is_uppercase() || c.is_ascii_digit()) {
-        return None
+    fn do_encode(c: u8, order: u8) -> u8 {
+        c + order
     }
-    let encoding = s.chars().zip(repeat(key.chars()).flat_map(|c| c)).map(|(c, k)| {
-        let order = k as u8 - A;
-        wrap(c as u8 + order)
-    }).collect::<String>();
-    Some(encoding)
+    transform(key, s, do_encode)
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
-    if key.is_empty() || key.chars().any(|c|c.is_uppercase() || c.is_ascii_digit()) {
-        return None
+    fn do_decode(c: u8, order: u8) -> u8 {
+        c - order
     }
-    let decoding = s.chars().zip(repeat(key.chars()).flat_map(|c| c)).map(|(c, k)| {
-        let order = k as u8 - A;
-        wrap(c as u8 - order)
-    }).collect::<String>();
+    transform(key, s, do_decode)
+}
+
+pub fn transform(key: &str, s: &str, apply: fn(u8, u8) -> u8) -> Option<String> {
+    if key.is_empty() || key.chars().any(|c| c.is_uppercase() || c.is_ascii_digit()) {
+        return None;
+    }
+    let decoding = s
+        .chars()
+        .zip(repeat(key.chars()).flat_map(|c| c))
+        .map(|(c, k)| {
+            let order = k as u8 - A;
+            wrap(apply(c as u8, order))
+        })
+        .collect::<String>();
     Some(decoding)
 }
 
@@ -34,23 +41,9 @@ pub fn encode_random(s: &str) -> (String, String) {
 
 pub fn wrap(c: u8) -> char {
     let c = match c {
-        c if c > Z =>  A + (c - (Z + 1)),
-        c if c < A =>  Z - ((A - 1) - c),
-        c => c
+        c if c > Z => A + (c - (Z + 1)),
+        c if c < A => Z - ((A - 1) - c),
+        c => c,
     };
     c as char
 }
-
-// func wrap(r rune) rune {
-// 	if r > 'z' {
-// 		// if passed 'z' returns to 'a'
-// 		r = 'a' + (r - ('z' + 1))
-// 	}
-//
-// 	if r < 'a' {
-// 		// if passed 'a' returns to 'z'
-// 		r = 'z' - (('a' - 1) - r)
-// 	}
-//
-// 	return r
-// }
