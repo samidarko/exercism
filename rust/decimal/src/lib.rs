dc7c0cd401310fc291be24b6ef0731e101d69ca1
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-11-01 10:32:00 +0100

    Rust: Decimal

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -1,16 +1,61 @@
-use num_bigint::{BigUint, ToBigUint};
+use std::cmp::Ordering;
+use num_bigint::{BigInt, ToBigInt};
 use num_traits::One;
 use num_integer::Integer;
+use std::ops::{Add, Mul, Sub};
 
+#[derive(Eq, Ord, Debug)]
 pub struct Decimal {
-    numerator: BigUint,
-    denominator: BigUint,
+    numerator: BigInt,
+    denominator: BigInt,
 }
 
 impl PartialEq for Decimal {
     fn eq(&self, other: &Self) -> bool {
-        let lcm = self.denominator.lcm(&other.denominator);
-        &self.numerator * (&self.denominator / &lcm) == &other.numerator * (&other.denominator / &lcm)
+        let (_denominator, self_numerator, other_numerator) = self.get_common_denominator(other);
+        self_numerator == other_numerator
+    }
+}
+
+impl PartialOrd for Decimal {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let (_denominator, self_numerator, other_numerator) = self.get_common_denominator(other);
+        self_numerator.partial_cmp(&other_numerator)
+    }
+}
+
+impl Add for Decimal {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let (denominator, self_numerator, other_numerator) = self.get_common_denominator(&other);
+        Self {
+            numerator: self_numerator + other_numerator,
+            denominator
+        }
+    }
+}
+
+impl Sub for Decimal {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let (denominator, self_numerator, other_numerator) = self.get_common_denominator(&other);
+        Self {
+            numerator: self_numerator - other_numerator,
+            denominator
+        }
+    }
+}
+
+impl Mul for Decimal {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            numerator: self.numerator * other.numerator,
+            denominator: self.denominator * other.denominator
+        }
     }
 }
 
@@ -18,18 +63,25 @@ impl Decimal {
     pub fn try_from(input: &str) -> Option<Decimal> {
         match input.split_once('.') {
             Some((left, right)) if right.trim_end_matches('0').is_empty() => Some(Self {
-                numerator: left.parse::<BigUint>().unwrap(),
+                numerator: left.parse::<BigInt>().unwrap(),
                 denominator: One::one(),
             }),
-            Some((left, right)) => {
-                let denominator = 10.to_biguint().unwrap().pow(right.len() as u32);
-                let numerator = left.parse::<BigUint>().unwrap();
+            Some((_left, right)) => {
+                let denominator = 10.to_bigint().unwrap().pow(right.len() as u32);
+                let numerator = input.replace(".", "").parse::<BigInt>().unwrap();
                 Some(Self {
-                    numerator: numerator * &denominator,
+                    numerator,
                     denominator,
                 })
             },
-            _ => None,
+            _ => Self::try_from(format!("{}.0", input).as_str()),
         }
+    }
+
+    fn get_common_denominator(&self, other: &Self) -> (BigInt, BigInt, BigInt) {
+        let denominator = self.denominator.lcm(&other.denominator);
+        let self_numerator = &self.numerator * (&denominator / &self.denominator);
+        let other_numerator = &other.numerator * (&denominator / &other.denominator);
+        (denominator, self_numerator, other_numerator)
     }
 }
