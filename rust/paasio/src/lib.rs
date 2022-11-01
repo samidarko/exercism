d98ed51e61121ff24403f0c65e65fe0b7aa93902
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-11-01 15:46:31 +0100

    Rust: PaaS I/O

--- rust/paasio/src/lib.rs
+++ rust/paasio/src/lib.rs
@@ -7,9 +7,6 @@ pub struct ReadStats<R> {
 }
 
 impl<R: Read> ReadStats<R> {
-    // _wrapped is ignored because R is not bounded on Debug or Display and therefore
-    // can't be passed through format!(). For actual implementation you will likely
-    // wish to remove the leading underscore so the variable is not ignored.
     pub fn new(wrapped: R) -> ReadStats<R> {
         Self {
             wrapped,
@@ -33,17 +30,11 @@ impl<R: Read> ReadStats<R> {
 
 impl<R: Read> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        match self.wrapped.read(buf) {
-            Ok(size) => {
-                self.total_ops += 1;
-                self.total_bytes += size;
-                Ok(size)
-            }
-            err => {
-                self.total_ops += 1;
-                err
-            }
-        }
+        self.total_ops += 1;
+        self.wrapped.read(buf).map(|bytes_count| {
+            self.total_bytes += bytes_count;
+            bytes_count
+        })
     }
 }
 
@@ -54,9 +45,6 @@ pub struct WriteStats<W> {
 }
 
 impl<W: Write> WriteStats<W> {
-    // _wrapped is ignored because W is not bounded on Debug or Display and therefore
-    // can't be passed through format!(). For actual implementation you will likely
-    // wish to remove the leading underscore so the variable is not ignored.
     pub fn new(wrapped: W) -> WriteStats<W> {
         Self {
             wrapped,
@@ -80,22 +68,14 @@ impl<W: Write> WriteStats<W> {
 
 impl<W: Write> Write for WriteStats<W> {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        match self.wrapped.write(buf) {
-            Ok(size) => {
-                self.total_ops += 1;
-                self.total_bytes += size;
-                Ok(size)
-            }
-            err => {
-                self.total_ops += 1;
-                err
-            }
-        }
+        self.total_ops += 1;
+        self.wrapped.write(buf).map(|bytes_count| {
+            self.total_bytes += bytes_count;
+            bytes_count
+        })
     }
 
     fn flush(&mut self) -> Result<()> {
-        let result = self.wrapped.flush();
-        self.total_ops += 1;
-        result
+        self.wrapped.flush()
     }
 }
