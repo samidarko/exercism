4a28ef105ccc5b854ad40f47a27b31fec52cbf75
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-11-02 19:10:42 +0100

    Rust: Diffie-Hellman (custom modular_pow function)

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,5 +1,3 @@
-use num_bigint::ToBigUint;
-use num_traits::cast::ToPrimitive;
 use rand::Rng;
 
 pub fn private_key(p: u64) -> u64 {
@@ -10,21 +8,27 @@ pub fn private_key(p: u64) -> u64 {
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
     // g.pow(a as u32) % p
-
-    g.to_biguint()
-        .unwrap()
-        .modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap())
-        .to_u64()
-        .unwrap()
+    modular_pow(g as u128, a, p as u128)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
     // b_pub.pow(a as u32) % p
+    modular_pow(b_pub as u128, a, p as u128)
+}
 
-    b_pub
-        .to_biguint()
-        .unwrap()
-        .modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap())
-        .to_u64()
-        .unwrap()
+pub fn modular_pow(mut base: u128, mut exponent: u64, modulus: u128) -> u64 {
+    if modulus == 1 {
+        return 0;
+    }
+
+    let mut r: u128 = 1;
+    base = base % modulus;
+    while exponent > 0 {
+        if exponent % 2 == 1 {
+            r = (r * base) % modulus;
+        }
+        base = (base * base) % modulus;
+        exponent = exponent >> 1;
+    }
+    r as u64
 }
