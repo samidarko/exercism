71012c8c8d414b04b906d4e1560001d3458d5172
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-12-21 09:32:31 +0100

    Go: Alphametics

--- go/alphametics/alphametics.go
+++ go/alphametics/alphametics.go
@@ -33,16 +33,42 @@ func NewWord(word string, letters map[string]int) (*Word, error) {
 }
 
 func (w *Word) Value() int {
-
-	result := 0
+	value := 0
 	position := len(w.word) - 1
 
 	for _, l := range w.word {
-		result += w.letters[string(l)] * pow(10, position)
+		value += w.letters[string(l)] * pow(10, position)
 		position--
 	}
 
-	return result
+	return value
+}
+
+func Combinations(L []int, r int) [][]int {
+	if r == 1 {
+		//Convert every item in L to List and
+		//Append it to List of List
+		temp := make([][]int, 0)
+		for _, rr := range L {
+			t := make([]int, 0)
+			t = append(t, rr)
+			temp = append(temp, [][]int{t}...)
+		}
+		return temp
+	} else {
+		res := make([][]int, 0)
+		for i := 0; i < len(L); i++ {
+			//Take only elements till i
+			// remember we do not care about position
+			perms := make([]int, 0)
+			perms = append(perms, L[:i]...)
+			for _, x := range Combinations(perms, r-1) {
+				t := append(x, L[i])
+				res = append(res, [][]int{t}...)
+			}
+		}
+		return res
+	}
 }
 
 func Solve(puzzle string) (map[string]int, error) {
@@ -50,44 +76,55 @@ func Solve(puzzle string) (map[string]int, error) {
 	if !valid {
 		return nil, errors.New("invalid puzzle")
 	}
-	fmt.Println(terms)
-	fmt.Println(sum)
 	fmt.Println(letters)
 
-	data := map[string]int{"B": 9, "I": 1, "L": 0}
+	for _, permutation := range Combinations([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, len(letters)) {
+		data := map[string]int{}
 
-	total := 0
+		for i, l := range letters {
+			data[string(l)] = permutation[i]
+		}
 
-	for _, term := range terms {
-		word, _ := NewWord(term, data)
-		total += word.Value()
-	}
+		fmt.Println(data)
 
-	result := Word{word: sum, letters: data}
+		total := 0
 
-	resultValue := result.Value()
+		for _, term := range terms {
+			word, _ := NewWord(term, data)
+			total += word.Value()
+		}
 
-	if total == resultValue {
-		return data, nil
+		result, _ := NewWord(sum, data)
+
+		resultValue := result.Value()
+
+		if total == resultValue {
+			return data, nil
+		}
+
 	}
 
 	return nil, fmt.Errorf("no solution")
 }
 
-func parse(puzzle string) ([]string, string, map[string]int, bool) {
+func parse(puzzle string) ([]string, string, string, bool) {
 	parts := strings.Split(puzzle, " == ")
 	if len(parts) != 2 {
-		return nil, "", nil, false
+		return nil, "", "", false
 	}
 	terms := strings.Split(parts[0], " + ")
 	sum := parts[1]
-	letters := map[string]int{}
+	letters := make([]rune, 0)
+	letterSet := map[rune]bool{}
 
 	for _, word := range append(terms, sum) {
 		for _, letter := range word {
-			letters[string(letter)] = 0
+			if !letterSet[letter] {
+				letters = append(letters, letter)
+			}
+			letterSet[letter] = true
 		}
 	}
 
-	return strings.Split(parts[0], " + "), sum, letters, true
+	return strings.Split(parts[0], " + "), sum, string(letters), true
 }
