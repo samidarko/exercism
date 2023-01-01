ec5363a3d6e4077d9c47807a74aaff09f43d453a
Vincent Dupont <vincent.dupont@trydatabook.com>
2023-01-01 23:05:28 +0100

    Go: Dominoes

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,83 +1,59 @@
 package dominoes
 
-import "sort"
+import (
+	"sort"
+)
 
 // Domino type here.
 type Domino [2]uint
-type Graph map[uint][]uint
 
-func MakeChain(dominos []Domino) ([]Domino, bool) {
+type Dominos []Domino
+
+func (receiver Dominos) Clone() Dominos {
+	dominos := Dominos{}
+
+	for _, domino := range receiver {
+		dominos = append(dominos, Domino{domino[0], domino[1]})
+	}
+
+	return dominos
+}
+
+func (receiver Dominos) Insert(position int, domino Domino) Dominos {
+	dominos := make(Dominos, 0)
+
+	for _, d := range receiver[:position] {
+		dominos = append(dominos, d)
+	}
+
+	dominos = append(dominos, domino)
+
+	for _, d := range receiver[position:] {
+		dominos = append(dominos, d)
+	}
+
+	return dominos
+}
+
+func MakeChain(dominos Dominos) (Dominos, bool) {
 	if len(dominos) == 0 {
 		return nil, true
 	}
-	isAllEvenDegrees := allEvenDegrees(dominos[:])
-	if !isAllEvenDegrees {
-		return nil, false
-	}
-	if isAllEvenDegrees && isConnected(dominos[:]) {
-		return createChain(dominos[:]), true
-		//return nil, true
-	} else {
-		return nil, false
-	}
+	chain := createChain(dominos.Clone())
+	return chain, len(chain) == len(dominos) && chain[0][0] == chain[len(chain)-1][1]
 }
 
-func getAdjacencyList(dominos []Domino) Graph {
-	graph := Graph{}
-	for _, domino := range dominos {
-		graph[domino[0]] = append(graph[domino[0]], domino[1])
-		graph[domino[1]] = append(graph[domino[1]], domino[0])
-
-	}
-	return graph
-}
-
-func allEvenDegrees(dominos []Domino) bool {
-	nodes := map[uint]uint{}
-	for _, domino := range dominos {
-		nodes[domino[0]]++
-		nodes[domino[1]]++
-	}
-	for _, count := range nodes {
-		if count%2 != 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func isConnected(dominos []Domino) bool {
-	graph := getAdjacencyList(dominos)
-	visited := make([]bool, len(graph), len(graph))
-	DepthFirstRecursive(graph, dominos[0][0], visited)
-	for _, isVisited := range visited {
-		if !isVisited {
-			return false
-		}
-	}
-	return true
-}
-
-func DepthFirstRecursive(graph Graph, node uint, visited []bool) {
-	visited[node-1] = true
-	for _, neighbor := range graph[node] {
-		if !visited[neighbor-1] {
-			DepthFirstRecursive(graph, neighbor, visited)
-		}
-	}
-}
-
-func createChain(dominos []Domino) []Domino {
+func createChain(dominos Dominos) Dominos {
 	// Sort the dominos in ascending order based on the values on their ends
 	sort.Slice(dominos, func(i, j int) bool {
 		if dominos[i][0] != dominos[j][0] {
 			return dominos[i][0] < dominos[j][0]
 		}
-		return dominos[i][1] < dominos[j][1]
+		return dominos[i][1] > dominos[j][1]
 	})
 
 	// Initialize the chain with the first domino in the sorted list
-	chain := []Domino{dominos[0]}
+	chain := Dominos{dominos[0]}
 	dominos = dominos[1:]
 
 	for len(dominos) > 0 {
@@ -100,10 +76,20 @@ func createChain(dominos []Domino) []Domino {
 				found = true
 				break
 			}
+			if domino[0] == domino[1] {
+				for j := 0; j < len(chain)-1; j++ {
+					if chain[j][1] == chain[j+1][0] && chain[j][1] == domino[0] {
+						chain = chain.Insert(j+1, domino)
+						dominos = append(dominos[:i], dominos[i+1:]...)
+						found = true
+						break
+					}
+				}
+			}
 		}
 		if !found {
 			// No match found, the input set of dominos cannot be used to form a valid chain
-			return nil
+			return chain
 		}
 	}
 
