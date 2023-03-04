eae5c34ae3c34e6f6663377e887bf7e47f83ed03
Vincent Dupont <vincent.dupont@trydatabook.com>
2023-03-04 13:44:24 +0100

    Go: Resistor Color Duo

---
+++ go/resistor-color-duo/resistor_color_duo.go
@@ -0,0 +1,35 @@
+package resistorcolorduo
+
+func Color(color string) int {
+	switch color {
+	case "black":
+		return 0
+	case "brown":
+		return 1
+	case "red":
+		return 2
+	case "orange":
+		return 3
+	case "yellow":
+		return 4
+	case "green":
+		return 5
+	case "blue":
+		return 6
+	case "violet":
+		return 7
+	case "grey":
+		return 8
+	case "white":
+		return 9
+	default:
+		return -1
+	}
+}
+
+// Value should return the resistance value of a resistor with a given colors.
+func Value(colors []string) (value int) {
+	value += Color(colors[0]) * 10
+	value += Color(colors[1])
+	return
+}
