66eeb1622c4d890b6536b8eb5c11ee4a443ae208
Vincent Dupont <vincent.dupont@trydatabook.com>
2023-03-15 18:04:41 +0100

    Rust: Robot Simulator

---
+++ rust/robot-simulator/src/lib.rs
@@ -0,0 +1,80 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+use crate::Direction::{East, North, South, West};
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Robot {
+    x: i32,
+    y: i32,
+    direction: Direction,
+}
+
+impl Robot {
+    pub fn new(x: i32, y: i32, direction: Direction) -> Self {
+        Self { x, y, direction }
+    }
+
+    #[must_use]
+    pub fn turn_right(self) -> Self {
+        let direction = match self.direction {
+            North => East,
+            East => South,
+            South => West,
+            West => North,
+        };
+        Self::new(self.x, self.y, direction)
+    }
+
+    #[must_use]
+    pub fn turn_left(self) -> Self {
+        let direction = match self.direction {
+            North => West,
+            West => South,
+            South => East,
+            East => North,
+        };
+        Self::new(self.x, self.y, direction)
+    }
+
+    #[must_use]
+    pub fn advance(self) -> Self {
+        let (x, y) = match self.direction {
+            North => (self.x, self.y + 1),
+            South => (self.x, self.y - 1),
+            East => (self.x + 1, self.y),
+            West => (self.x - 1, self.y),
+        };
+        Self::new(x, y, self.direction)
+    }
+
+    #[must_use]
+    pub fn instructions(self, instructions: &str) -> Self {
+        let mut robot = self;
+        for instruction in instructions.chars() {
+            match instruction {
+                'R' => robot = robot.turn_right(),
+                'L' => robot = robot.turn_left(),
+                'A' => robot = robot.advance(),
+                _ => unreachable!(),
+            };
+        }
+        robot
+    }
+
+    pub fn position(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    pub fn direction(&self) -> &Direction {
+        &self.direction
+    }
+}
