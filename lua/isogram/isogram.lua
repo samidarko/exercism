40720fa2d29aa3ade157e824c5981ac4dd767e0a
Vincent Dupont <vincent.dupont@trydatabook.com>
2023-04-18 19:50:07 +0200

    Lua: Isogram

--- lua/isogram/isogram.lua
+++ lua/isogram/isogram.lua
@@ -1,13 +1,10 @@
 return function(s)
-    s = string.lower(s)
     local chars = {}
-    for c in s:gmatch "." do
-        if c ~= "-" and c ~= " " then
-            if chars[c] then
-                return false
-            end
-            chars[c] = true
+    for c in s:lower():gmatch "%a" do
+        if chars[c] then
+            return false
         end
+        chars[c] = true
     end
 
     return true
